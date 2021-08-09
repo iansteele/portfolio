30885a0e90b3c4a19975ae724c69db6c8391e3af
isteele <steelecreativefactory@gmail.com>
2021-08-08 21:48:29 -0500

    Actually really like where colors & type are going

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Button(props) {
   return (
-    <a className="px-4 py-2 rounded mt-2 align-center bg-brand-secondary text-brand-primary ring-current font-bold w-max" href={props.link}>
+    <a className="px-4 py-2 rounded mt-2 align-center bg-brand-secondary text-brand-primary ring-current text-sm font-bold w-max" href={props.link}>
       {props.label}
     </a>
   );
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function Thumbnail(props) {
   return (
-      <Link className="transform transition duration-500 hover:ring hover:ring-brand-secondary hover:ring-offset-2 hover:scale-101 rounded-3xl relative overflow-hidden " to={props.link}>
+      <Link className="transform transition duration-500 hover:ring hover:ring-brand-secondary hover:ring-offset-2 hover:scale-101 rounded-xl relative overflow-hidden " to={props.link}>
         <img className="" src={process.env.PUBLIC_URL + "/"+ props.image} alt="Project Image"/>
         <div className="bg-brand-primary rounded-full  absolute bottom-2 right-2 py-2 px-4">
           <div className="project-title text-xs font-semibold">
