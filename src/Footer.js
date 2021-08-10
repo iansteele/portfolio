bfc8b4ac308cb82c98a4419db864d9ec3103b18d
isteele <steelecreativefactory@gmail.com>
2021-08-10 08:23:08 -0500

    adjusted footer spacing

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import Button from './Button.js';
 
 function Footer(props) {
   return (
-    <footer className="flex flex-col space-y-10 xs:space-y-0 text-white  sm:flex-row items-center justify-between text-gray-900 mt-8 px-6 py-8">
+    <footer className="flex flex-col space-y-6 xs:space-y-0 text-white  xs:flex-row items-center justify-between text-gray-900 mt-8 sm:mt-0 px-6 py-10">
       <div className="flex flex-col items-center sm:items-start space-y-4 ">
         <p className="text-sm">Want to work together?</p>
         <Button className="bg-brand-secondary text-brand-primary"
--- src/Hero.js
+++ src/Hero.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Hero(props) {
   return (
-    <div className="flex-grow flex flex-col text-center items-center text-white pb-12 pt-16 md:pb-16 px-6 md:pt-24  xl:pb-24 xl:pt-24 bg-gray bg-opacity-5 rounded-xl m-4 md:m-6 mt-0">
+    <div className="flex-grow flex flex-col text-center items-center text-white pb-12 pt-16 md:pb-16 px-6 md:pt-24 xl:pb-24 xl:pt-24 bg-gray bg-opacity-5 rounded-xl m-4 mt-0 md:m-6 md:mt-0">
       <img className="w-24 h-24 md:w-32 md:h-32 rounded-full" src={process.env.PUBLIC_URL + "/img/ian2.png"} alt=""/>
       <h1 className="text-5xl md:text-7xl font-display font-bold mt-4 tracking-wide">Hi, I'm Ian!</h1>
       <p className="text-lg mt-4 ">I'm crafting a K-12 curriculum platform with the amazing folks @ <a className="text-brand-secondary font-semibold hover:text-white" href="https://www.learnzillion.com">LearnZillion</a></p>
