5e80eccb762b80ccb957769c92f7f572bf888fe1
isteele <steelecreativefactory@gmail.com>
2021-08-13 22:49:55 -0500

    adds grading project

--- src/projects/Grading.js
+++ src/projects/Grading.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import CaseStudy from './CaseStudy.js';
 
 function Grading(props) {
   return (
-    <CaseStudy title="Grading Assignments" company="LearnZillion">
-
-    </CaseStudy>
+    <div className="max-w-screen-lg px-6 mx-auto mt-16 text-white">
+      <h2 className="text-5xl text-white font-display">Grading Assignments</h2>
+      <p className="mt-2 font-bold text-brand-secondary">LearnZillion</p>
+      <ul className="flex mt-2 space-x-4 text-xs text-gray">
+        <li>UX/UI</li>
+        <li></li>
+      </ul>
+      <img className="mt-10" src={process.env.PUBLIC_URL + "/img/manualgrading-bg.jpg"} alt="Redesigned reports page"/>
+      <div className="mt-10 max-w-prose">
+        <h3 className="text-3xl text-brand-secondary font-display">The Challenge</h3>
+        <p className="mt-4 leading-5"> When I began working at LearnZillion, our assessments allowed for single-part questions.
+        </p>
+        <p className="mt-8 leading-5">
+          After integrating a feature-rich assessment authoring platform, LearnZillion broke free from only being able to have one item per question. Instead, a single question could now have multiple parts which could be any combination of auto-graded and/or manually graded.</p>
+        <p className="mt-8 leading-5">We needed a way to quickly let see teachers how their classes are doing, while allowing them to easily see which questions required their attention.</p>
+      </div>
+      <div className="mt-16">
+        <img src={process.env.PUBLIC_URL + "/img/grading-original.png"} alt="Redesigned reports page"/>
+        <span className="mt-8 text-xs text-gray">Original Design</span>
+      </div>
+      <div className="mt-10 max-w-prose">
+        <h3 className="text-3xl text-brand-secondary font-display">User insights</h3>
+          <p className="mt-8"> On average, teachers spent 12 hours each week on non-compensated school-related activities such as grading papers, bus duty, and club advising. <span className="text--credit">- National Education Association</span></p>
+          <p className="mt-4">We needed a way to quickly let see teachers how their classes are doing, while allowing them to easily see which questions required their attention.</p>
+      </div>
+      <div className="mt-10 max-w-prose">
+        <h3 className="text-3xl text-brand-secondary font-display">Concerns</h3>
+        <div className="mt-4">
+          <p className="mt-8">Reports listed all classes together. This made it difficult
+            for teachers to distinguish how their individual classrooms were
+            performing, and prevented teachers from knowing which class each student belonged to.
+          </p>
+          <p className="mt-4">Assignment questions can now be multi-part. How do we display
+            completion, scoring, and need for manual grading?
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 max-w-prose">
+        <h3 className="text-3xl text-brand-secondary font-display">Ideation</h3>
+        <p className="mt-8">I began sketching ways to funnel teachers to the reports page when an assignment required their attention. Concepts included the use of notifications, visual cues in the site nav, or allowing for the creation of due-dates when assigning. However, a restriction of our third party integration limited us to only being able to pull 50 student reports at a time. Once there, a system of colors and shapes provided a quick reference to student performance instead of misleading scores. Allowing teachers to toggle grading by question instead of by student saved teachers time, allowing them to focus additional learning where necessary</p>
+      </div>
+      <div className="flex flex-wrap mt-10">
+        <img src={process.env.PUBLIC_URL + "/img/gradingflow-sketch.png"} alt="Redesigned reports page"/>
+        <img src={process.env.PUBLIC_URL + "/img/gradingmodal-sketch.png"} alt="Redesigned reports page"/>
+        <img src={process.env.PUBLIC_URL + "/img/gradingicon-sketch.png"} alt="Redesigned reports page"/>
+      </div>
+      <div className="mt-10 max-w-prose">
+        <h3 className="text-3xl text-brand-secondary font-display">The Outcome</h3>
+        <p className="mt-8">The final functionality of this feature included the ability to switch between grading by student, or by question. The ability to switch to grading by question allowed for teachers to quickly grade all items that required their attention. Giving them more time to prepare for subsequent lessons, or to help those students not at expectation. Following up with users after release we learned that teachers had no difficulty discerning which items required their manual review.</p>
+      </div>
+      <div className="mt-10">
+        <img className="w-full" src={process.env.PUBLIC_URL + "/img/grading_table-row.png"} alt="Redesigned reports page"/>
+        <img className="w-full" src={process.env.PUBLIC_URL + "/img/grading-modal.png"} alt="Redesigned reports page"/>
+      </div>
+    </div>
   )
 }
 
--- src/projects/VirtualFest.js
+++ src/projects/VirtualFest.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import CaseStudy from './CaseStudy.js';
 
 function VirtualFest(props) {
   return (
-    <CaseStudy title="Virtual Fest" company="Untappd">
-      <ul className="flex mt-2 space-x-4 text-xs text-gray"><li>Illustration</li><li>Web Design</li></ul>
-      <div className="mt-10">
+    <div className="max-w-screen-lg px-6 mx-auto mt-16 text-white">
+      <h2 className="text-5xl text-white font-display">Virtual Fest</h2>
+      <p className="mt-2 font-bold text-brand-secondary">Untappd</p>
+      <ul className="flex mt-2 space-x-4 text-xs text-gray">
+        <li>Illustration</li>
+        <li>Web Design</li>
+      </ul>
+      <div className="mt-10 max-w-prose">
         <h3 className="text-3xl text-brand-secondary font-display">The Challenge</h3>
         <p className="mt-4 leading-5 "> During the middle of the COVID-19 pandemic, Untappd cancelled the 2nd Annual
           Beer Festival. Given that the service industry had been hit so hard, Untappd
@@ -20,15 +24,17 @@ function VirtualFest(props) {
           explored the organization of page content.
         </p>
       </div>
-      <img className="mt-4" src={process.env.PUBLIC_URL + "/img/ut/v-fest/hero.jpg"} alt="Redesigned reports page"/>
+      <img className="mt-20" src={process.env.PUBLIC_URL + "/img/ut/v-fest/hero.jpg"} alt="Redesigned reports page"/>
       <img  src={process.env.PUBLIC_URL + "/img/ut/v-fest/howitworks.jpg"} alt="Redesigned reports page"/>
       <div className="mt-10 section">
-        <h3 className="text-3xl text-brand-secondary font-display">The Outcome</h3>
-        <p className="mt-4 text-sm leading-5 ">The Untappd Virtual fest was conceptualized, negotiated, put on sale,
+        <div className="max-w-prose">
+          <h3 className="text-3xl text-brand-secondary font-display">The Outcome</h3>
+          <p className="mt-4 text-sm leading-5 ">The Untappd Virtual fest was conceptualized, negotiated, put on sale,
           developed, curated, rehearsed, and executed in 50 days. It took a ton of
           effort and communication across teams, but together we were able to accomplish
           the following:
-        </p>
+          </p>
+        </div>
         <div className="flex mt-10">
           <div className="flex flex-col items-center flex-grow ">
             <h2>$12,000+</h2>
@@ -43,9 +49,9 @@ function VirtualFest(props) {
             <p className="text-gray">Chat messages</p>
           </div>
         </div>
-        <img className="mt-16" src={process.env.PUBLIC_URL + "/img/ut/v-fest/fullpage.png"} alt="Redesigned reports page"/>
+        <img className="mt-20" src={process.env.PUBLIC_URL + "/img/ut/v-fest/fullpage.png"} alt="Redesigned reports page"/>
       </div>
-    </CaseStudy>
+    </div>
   )
 }
 
