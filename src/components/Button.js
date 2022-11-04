adc42028c76a6507c704befec29b4af80e32d23a
isteele <steelecreativefactory@gmail.com>
2022-11-04 12:31:09 -0500

    Updates position of hover state on projects

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
-import React from 'react'; // Import the Component component from React
-import '../App.css';
+import React from "react"; // Import the Component component from React
+import "../App.css";
 
 function Button(props) {
   return (
-    <a className="px-4 py-2 mt-2 text-sm font-bold transition-colors rounded align-center bg-brand-secondary text-brand-primary ring-current w-max hover:bg-white" href={props.link}>
+    <a
+      className="px-4 py-2 mt-2 text-sm font-bold transition-colors rounded outline-none focus:bg-brand-tertiary focus:ring align-center bg-brand-secondary text-brand-primary ring-brand-secondary w-max hover:bg-white"
+      href={props.link}
+    >
       {props.label}
     </a>
   );
