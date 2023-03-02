2e48db1a25b8c4ff6fff83d1eae9eda6ed0e05c6
isteele <steelecreativefactory@gmail.com>
2023-03-02 07:27:40 -0600

    Deploy

---
+++ out/_next/static/chunks/pages/About-2a6dbbce05904a4d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[921],{4741:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/About",function(){return t(8045)}])},8045:function(n,u,t){"use strict";t.r(u);var _=t(5893);t(7294),u.default=function(){return(0,_.jsx)("h1",{children:"Hello World!"})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4741)}),_N_E=n.O()}]);
