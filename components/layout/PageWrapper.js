cacab04d75ef72296efac708f4478f2ef06e2c4e
isteele <steelecreativefactory@gmail.com>
2023-03-03 22:42:13 -0600

    adjusts flex properties for pagewrapper

--- components/layout/PageWrapper.js
+++ components/layout/PageWrapper.js
@@ -15,7 +15,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
     ...customMeta,
   };
   return (
-    <div className="flex flex-col items-center bg-brand-primary mx-auto">
+    <div className="flex flex-col items-stretch mx-auto bg-brand-primary">
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
