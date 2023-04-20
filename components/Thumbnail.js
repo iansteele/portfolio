84bfce91d86a8f4374486ca9cb4cb8dfc78ac653
isteele <steelecreativefactory@gmail.com>
2023-04-19 22:28:48 -0500

    Fixed outline

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ function Thumbnail(props) {
   const [isLoading, setLoading] = useState(true);
   return (
     <li>
-      <Link className="group" href={props.link}>
+      <Link className="outline-none group" href={props.link}>
         <div className="relative flex flex-col w-full h-full p-2 space-y-4 transition duration-500 transform border shadow-md group-hover:-translate-y-1 shadow-neutral-300 border-neutral-300 group-hover:border-opacity-90 group-active:translate-y-0 group-active:shadow-none group-hover:shadow-lg dark:border-slate-700 dark:shadow-black bg-gradient-to-b dark:from-slate-700 from-neutral-100 to-neutral-50 dark:to-brand-primary aspect-auto rounded-xl focus-base">
           <div className="w-full h-auto overflow-hidden rounded-md will-change-transform">
             <Image
