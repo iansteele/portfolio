48ea82811465eb02cbd2796b5d731e1619c9afb4
isteele <steelecreativefactory@gmail.com>
2023-06-28 08:45:22 -0500

    Tries again to fix the safari height bug

--- components/layout/PageWrapper.js
+++ components/layout/PageWrapper.js
@@ -49,7 +49,7 @@ export default function PageWrapper({ children, ...customMeta }) {
       </Head>
 
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
       <ThemeButton />
     </div>
