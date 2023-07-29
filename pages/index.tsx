9573dc41616d1394413e06bb326296f032ea497d
isteele <steelecreativefactory@gmail.com>
2023-07-29 16:41:06 -0500

    Fixes hero size

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,10 @@ export default function Home() {
             rel="stylesheet"
           />
         </Head>
-        <div className="relative flex items-center flex-grow w-full max-w-screen-xl gap-4 px-6 max-h-[700px] h-[33vh] md:h-[50vh] lg:h-[66vh] py-8 md:items-center md:justify-start text-brand-tertiary md:flex-row">
-          <div className="relative flex flex-col flex-grow w-full min-w-0 max-w-prose">
-            <h1 className="tracking-tight text-8xl">Hi, I'm Ian</h1>
-            <p className="mt-10 text-2xl text-brand-tertiary-muted max-w-prose">
+        <div className="relative flex flex-col items-center flex-grow w-full max-w-screen-xl gap-10 px-6 py-10 h-fit min-h-[50vh] md:items-start md:justify-start text-brand-tertiary">
+          <div className="relative flex flex-col justify-center flex-grow w-full min-w-0 max-w-prose">
+            <h1 className="tracking-tight lg:text-8xl">Hi, I'm Ian</h1>
+            <p className="lg:text-2xl text-brand-tertiary-muted max-w-prose">
               I'm product designer passionate about{" "}
               <span className="highlight">design systems</span>,{" "}
               <span className="highlight">front-end development</span> and{" "}
@@ -35,8 +35,8 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="absolute -mt-4 transform -translate-x-1/2 left-1/2 bottom-2 ">
-            <div className="flex items-center justify-center flex-shrink-0 p-2 origin-top bg-opacity-50 rounded-full shadow-zinc-900 animate-bounce bg-brand-secondary border-brand-primary text-brand-primary ">
+          <div className="absolute self-center bottom-2">
+            <div className="flex items-center justify-center flex-shrink-0 p-2 origin-top bg-opacity-50 rounded-full shadow-zinc-900 animate-bounce bg-brand-muted border-brand-primary text-brand-tertiary ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
