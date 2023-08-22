eed93ccfb93f96e32210941035b85ada565f77ad
isteele <steelecreativefactory@gmail.com>
2023-08-22 14:36:38 -0500

    Updates borders, rotates them thumbs

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ function Thumbnail(props) {
   return (
     <li className="opacity-100">
       <Link className="outline-none group" href={props.link}>
-        <div className="relative flex flex-col w-full h-full p-2 transition duration-500 transform bg-white border dark:bg-brand-muted border-neutral-200 group-hover:-translate-y-1 group-hover:shadow-slate-200 group-hover:border-opacity-90 group-active:translate-y-0 dark:border-zinc-800 group-active:shadow-none group-hover:shadow-lg dark:group-hover:shadow-slate-800 dark:group-hover:border-slate-700 aspect-auto rounded-xl focus-base">
+        <div className="relative flex flex-col w-full h-full p-2 transition-all duration-500 bg-white border transform-gpu group-hover:rotate-[0.5deg] dark:bg-brand-muted border-neutral-200 group-hover:-translate-y-1 group-hover:shadow-slate-200 group-hover:border-opacity-90 group-active:translate-y-0 dark:border-zinc-800 group-active:shadow-none group-hover:shadow-lg dark:group-hover:shadow-emerald-400/50 dark:group-hover:border-brand-secondary aspect-auto rounded-xl focus-base">
           <div className="w-full h-auto overflow-hidden rounded-md will-change-transform">
             <Image
               src={props.image}
