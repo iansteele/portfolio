09ee7b42951b1f0f05f2e7c3488fc1fd73ce5718
isteele <steelecreativefactory@gmail.com>
2023-10-04 16:48:05 -0500

    Typography fix in home hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,13 +28,13 @@ export default function Home() {
         <div className="relative flex flex-col items-center flex-grow w-full max-w-screen-xl gap-10 px-6 py-10 h-fit min-h-[50vh] md:items-start md:justify-start text-brand-tertiary">
           <div className="relative flex flex-col justify-center flex-grow w-full min-w-0 max-w-prose">
             <h1 className="tracking-tight lg:text-7xl">Hi, I'm Ian</h1>
-            <p className="text-2xl text-brand-tertiary-muted max-w-prose whitespace-nowrap">
+            <p className="text-2xl text-brand-tertiary-muted max-w-prose ">
               I'm a product designer passionate about{" "}
               <span className="font-semibold underline underline-offset-2 decoration-brand-secondary">
                 design systems
               </span>
               ,{" "}
-              <span className="font-semibold underline underline-offset-2 decoration-brand-secondary">
+              <span className="font-semibold underline whitespace-nowrap underline-offset-2 decoration-brand-secondary">
                 front-end development
               </span>{" "}
               and{" "}
