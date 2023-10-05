240e00a1e1be9f8580bdc047e16cb51ca3f16e8e
isteele <steelecreativefactory@gmail.com>
2023-10-04 23:24:36 -0500

    Fix content shift

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -32,11 +32,11 @@ function Thumbnail(props) {
         </div>
         <div className="absolute inset-0 flex flex-col w-full h-full overflow-hidden transition-all bg-brand-primary aspect-auto rounded-xl group-active:shadow-none transform-gpu group-hover:-translate-y-1 group-hover:border-opacity-90 group-active:translate-y-0"></div>
         <div className="relative flex flex-col w-full h-full p-2 transition-all duration-500 bg-white border transform-gpu dark:bg-brand-muted border-neutral-200 group-hover:-translate-y-1 group-hover:shadow-slate-200 group-hover:border-opacity-90 group-active:translate-y-0 dark:border-zinc-800 group-active:shadow-none group-hover:shadow-lg dark:group-hover:shadow-emerald-400/50 aspect-auto rounded-xl focus-base">
-          <div className="flex w-full h-auto overflow-hidden rounded-md will-change-transform">
+          <div className="flex w-full h-auto overflow-hidden aspect-[4/3] rounded-md min-h-max will-change-transform">
             <Image
               src={props.image}
               alt={props.alt}
-              className={`overflow-hidden pointer-events-none flex-shrink ease-in-out aspect-auto w-full transition-transform duration-500 transform-gpu group-hover:scale-101 group-focus-visible:scale-101`}
+              className={`overflow-hidden pointer-events-none flex-shrink ease-in-out aspect-auto min-h-max w-full transition-transform duration-500 transform-gpu group-hover:scale-101 group-focus-visible:scale-101`}
               width="400"
               height="300"
             />
