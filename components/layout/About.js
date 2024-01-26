ef7714554fb32d2f8f9c2ecb9771a1725c6e8fde
isteele <steelecreativefactory@gmail.com>
2024-01-25 21:42:41 -0600

    Updates hero

--- components/layout/About.js
+++ components/layout/About.js
@@ -4,12 +4,12 @@ import Image from "next/image";
 export default function About() {
   return (
     <div className="w-full bg-brand-muted ">
-      <div className="grid w-full grid-cols-1 gap-4 px-4 mx-auto space-y-0 list-none max-w-screen-2xl place-items-stretch auto-rows-max md:grid-cols-2 lg:grid-cols-3 ">
+      <div className="grid w-full grid-cols-1 gap-10 px-4 mx-auto space-y-0 list-none lg:gap-20 max-w-screen-2xl place-items-stretch auto-rows-max md:grid-cols-2 lg:grid-cols-3 ">
         <div className="grid w-full mt-24 place-self-start">
-          <div className="relative flex flex-shrink-0 w-1/2 col-span-1 mx-auto overflow-hidden rounded-full md:w-2/3 aspect-square ring-8 ring-brand-muted min-w-max bg-brand-tertiary dark:bg-brand-primary">
+          <div className="relative flex flex-shrink-0 w-full col-span-1 mx-auto aspect-square min-w-max">
             <Image
               className="w-full h-full aspect-square"
-              src="/img/ian-splatters-2.png"
+              src="/img/hero.png"
               alt="A low-fidelity sketch of early grading layouts"
               fill
               sizes="(max-width: 480px) 100vw"
@@ -20,7 +20,7 @@ export default function About() {
         <div className="flex flex-col flex-grow col-span-1 pt-10 pb-20 lg:col-span-2 md:pt-24 max-w-prose">
           <h1> About Me</h1>
           <p>
-            I am a principal product designer with 11+ years of experience based
+            I am a principal product designer with 12+ years of experience based
             in Illinois. I am working remotely on the{" "}
             <span className="font-bold">Classroom</span> team at{" "}
             <span className="font-bold whitespace-nowrap ">
@@ -36,26 +36,11 @@ export default function About() {
             inner-workings, learning the hot-keys, problem-solving, and pursuing
             challenges of all types.
           </p>
-          {/* <p className="mt-4 opacity-75 lg:text-xl text-brand-tertiary-muted max-w-prose">
-            I geek out about{" "}
-            <span className="font-semibold ">design systems</span>,{" "}
-            <span className="font-semibold whitespace-nowrap ">
-              front-end development
-            </span>{" "}
-            and <span className="font-semibold ">illustration</span>.
-          </p> */}
           <p>
             Want to know anything else? Feel free to{" "}
             <a href="steeledesignco@gmail.com">reach out</a>!
           </p>
         </div>
-        {/* <Image
-        src="/img/hero.png"
-        alt="A low-fidelity sketch of early grading layouts"
-        width={200}
-        height={200}
-        className="w-full mx-auto rounded-lg md:w-auto"
-      /> */}
       </div>
     </div>
   );
