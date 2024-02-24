caf8fba2c1bf87a794b48375c1f167736cfe6ae4
isteele <steelecreativefactory@gmail.com>
2024-02-24 07:13:22 -0600

    Adds key metric component

--- components/layout/Sections.js
+++ components/layout/Sections.js
@@ -12,4 +12,28 @@ function SectionHeading(props) {
   return <h2 className="inline-flex max-w-prose">{props.children}</h2>;
 }
 
-export { SectionWrapper, SectionHeading };
+function KeyMetrics(props) {
+  const { metrics = [] } = props;
+
+  console.log("Metrics:", metrics);
+
+  return (
+    <SectionWrapper>
+      <SectionHeading>Key metrics</SectionHeading>
+      <ul className="grid w-full gap-2 pl-0 list-none md:grid-cols-3">
+        {metrics.map((metric, index) => (
+          <li key={index} className="flex flex-col m-0">
+            <span className="opacity-50 text-brand-tertiary">
+              {metric.label}
+            </span>
+            <span className="text-5xl font-bold leading-none">
+              {metric.metric}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </SectionWrapper>
+  );
+}
+
+export { SectionWrapper, SectionHeading, KeyMetrics };
