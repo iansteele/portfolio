f3e71d4e1a640c22e5b3c25396da14113d28efaa
isteele <steelecreativefactory@gmail.com>
2024-03-21 20:19:31 -0500

    About me updates

--- pages/About.js
+++ pages/About.js
@@ -5,9 +5,9 @@ import { PageWrapper } from "../components/ComponentIndex";
 function App() {
   return (
     <PageWrapper>
-      <div className="flex flex-col items-center gap-20 px-0 py-8 max-w-7xl md:items-start">
-        <div className="flex flex-col px-6 lg:w-2/3 max-w-prose">
-          <h1 className="w-full mx-auto md:mx-0"> About Me</h1>
+      <div className="flex flex-col items-center flex-shrink-0 gap-20 px-0 py-8 max-w-7xl md:items-start">
+        <div className="flex flex-col px-6 max-w-prose">
+          <h1 className="w-full mx-auto mt-16 md:mx-0"> About Me</h1>
           <p>
             I am a principal product designer based in Illinois. Currently, I am
             on the <span className="font-bold">Classroom</span> team at{" "}
@@ -31,45 +31,58 @@ function App() {
           <h2 className="mt-20">But like... what about NON-work stuff? </h2>
           <p>
             I am a husband, father of 3, lover of the outdoors, huge fan of all
-            things spooky, and a 4-stripe white belt in Brazillian Jiu Jitsu. If
-            I could only eat 🌮 for the rest of my life, I'd be perfectly ok
-            with it.
+            things spooky, and a blue belt in Brazillian Jiu Jitsu. If I could
+            only eat 🌮 for the rest of my life, I'd be perfectly ok with it.
           </p>
           <p className="mt-10">
             Want to know anything else? Feel free to reach out!
           </p>
         </div>
-        <div className="flex gap-4 px-4 pb-4 overflow-x-scroll lg:w-full scroll-smooth">
-          <div className="flex flex-row w-full gap-4 ">
-            <Image
-              src="/img/About/beach.jpeg"
-              alt="Me and my family at the beach"
-              width={400}
-              height={400}
-              className="w-3/4 flex-shrink-0 lg:w-1/5 max-w-[400px] shadow-lg lg:max-w-4xl rounded-lg"
-            />
-            <Image
-              src="/img/About/bjj.jpeg"
-              alt="Receiving my bluebelt in brazilian jiu jitsu"
-              width={400}
-              height={400}
-              className="w-3/4 flex-shrink-0 lg:w-1/5 max-w-[400px] shadow-lg lg:max-w-4xl rounded-lg"
-            />
-            <Image
-              src="/img/About/sisters.jpeg"
-              alt="A photo of Ian's daughter and her best friend... her dog."
-              width={400}
-              height={400}
-              className="w-3/4 flex-shrink-0 lg:w-1/5 max-w-[400px] shadow-lg lg:max-w-4xl rounded-lg"
-            />
-            <Image
-              src="/img/About/rollercoaster.jpeg"
-              alt="Ian's family in front of the Hulk at Universal Studios Orlando"
-              width={400}
-              height={400}
-              className="w-3/4 flex-shrink-0 lg:w-1/5 max-w-[400px] shadow-lg lg:max-w-4xl rounded-lg"
-            />
-          </div>
+      </div>
+      <div className="flex justify-center w-full gap-4 px-4 pb-4 mt-20 overflow-x-scroll scroll-smooth">
+        <div className="flex flex-row gap-4 justify-self-center ">
+          <Image
+            src="/img/About/beach.jpeg"
+            alt="Me and my family at the beach"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
+          <Image
+            src="/img/About/bjj.jpeg"
+            alt="Receiving my bluebelt in brazilian jiu jitsu"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
+          <Image
+            src="/img/About/draplin.jpeg"
+            alt="Ian's family in front of the Hulk at Universal Studios Orlando"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
+          <Image
+            src="/img/About/soccer.jpeg"
+            alt="Me doing my best to wrangle kids as a pre-k soccer coach"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
+          <Image
+            src="/img/About/rollercoaster.jpeg"
+            alt="Ian's family in front of the Hulk at Universal Studios Orlando"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
+          <Image
+            src="/img/About/tm.jpeg"
+            alt="Ian's family in front of the Hulk at Universal Studios Orlando"
+            width={400}
+            height={400}
+            className="flex-shrink-0 w-1/2 rounded-lg shadow-lg lg:w-1/4 aspect-sqaure lg:max-w-xl"
+          />
         </div>
       </div>
     </PageWrapper>
