0b0af10857e54566159f7ba392ef1f65c883dda3
isteele <steelecreativefactory@gmail.com>
2025-07-25 13:24:30 -0500

    Fixes icon bug in hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,6 +74,7 @@ export default function Home() {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
+                  className="w-full h-full"
                 >
                   <path d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z" />
                 </svg>
